a41b63d1980f05d7382cf0f931948559f14fdc26
HeadHodge <7340822+HeadHodge@users.noreply.github.com>
2021-07-05 16:10:14 -0700

    update

--- smartRemotes/imports/ble/hidKeyboard.py
+++ smartRemotes/imports/ble/hidKeyboard.py
@@ -29,132 +29,116 @@ class HIDService(gattApplication.Service):
     def __init__(self, bus, index):
         gattApplication.Service.__init__(self, bus, index, self.SERVICE_UUID, True)
         
-        self.protocolMode = ProtocolModeCharacteristic(bus, 0, self)
-        self.hidInfo = HIDInfoCharacteristic(bus, 1, self)
-        self.controlPoint = ControlPointCharacteristic(bus, 2, self)
-        self.reportMap = ReportMapCharacteristic(bus, 3, self)
-        self.report1 = Report1Characteristic(bus, 4, self)
-        self.report2 = Report2Characteristic(bus, 5, self)
+        self.report = ReportCharacteristic(bus, 0, self)
+        self.reportMap = ReportMapCharacteristic(bus, 1, self)
+        self.hidInfo = HIDInfoCharacteristic(bus, 2, self)
+        self.protocolMode = ProtocolModeCharacteristic(bus, 3, self)
+        #self.controlPoint = ControlPointCharacteristic(bus, 4, self)
+        #self.report1 = Report1Characteristic(bus, 5, self)
         
-        self.add_characteristic(self.protocolMode)
-        self.add_characteristic(self.hidInfo)
-        self.add_characteristic(self.controlPoint)
+        self.add_characteristic(self.report)
         self.add_characteristic(self.reportMap)
-        self.add_characteristic(self.report1)
-        self.add_characteristic(self.report2)
-        
-#name="Protocol Mode" sourceId="org.bluetooth.characteristic.protocol_mode" uuid="2A4E"
-class ProtocolModeCharacteristic(gattApplication.Characteristic):
+        self.add_characteristic(self.hidInfo)
+        self.add_characteristic(self.protocolMode)
+        #self.add_characteristic(self.report1)
+        #self.add_characteristic(self.controlPoint)
 
-    CHARACTERISTIC_UUID = '2A4E'
+
+#id="report" name="Report" sourceId="org.bluetooth.characteristic.report" uuid="2A4D"        
+class ReportCharacteristic(gattApplication.Characteristic):
+
+    CHARACTERISTIC_UUID = '2A4D'
 
     def __init__(self, bus, index, service):
-        
         gattApplication.Characteristic.__init__(
                 self, bus, index,
                 self.CHARACTERISTIC_UUID,
-                ["read", "write-without-response"],
+                ['read', 'notify'],
                 service)
                 
-        '''        
-        <Field name="Protocol Mode Value">
+        '''
+        <Field name="Report Value">
         <Requirement>Mandatory</Requirement>
         <Format>uint8</Format>
-        <Enumerations>
-        <Enumeration key="0" value="Boot Protocol Mode"/>
-        <Enumeration key="1" value="Report Protocol Mode"/>
-        <ReservedForFutureUse start="2" end="255"/>
-        </Enumerations>
+        <Repeated>true</Repeated>
+        </Field>
+        
+        Use standard key codes: https://www.usb.org/sites/default/files/documents/hut1_12v2.pdf
         '''
         
-        #self.value = dbus.Array([1], signature=dbus.Signature('y'))
-        self.parent = service
-        self.value = dbus.Array(bytearray.fromhex('01'), signature=dbus.Signature('y'))
-        #print(f'***ProtocolMode value***: {self.value}')
-
+        self.add_descriptor(ReportReferenceDescriptor(bus, 1, self))
+        
+        self.isConnected = False
+        self.value = [dbus.Byte(0x00),dbus.Byte(0x00)]
+        #print(f'***Report value***: {self.value}')
+              
+    def sendKey(self, keyBytes, keyHold):
+        #send keyCode
+        print(f' \n***Send report2 keyCode: {[hex(x) for x in keyBytes]}, keyHold: {keyHold}***');
+        #if(self.isConnected == False): print('Abort Report2: Not connected to client'); return
+        self.PropertiesChanged(GATT_CHRC_IFACE, { 'Value': [dbus.Byte(keyBytes[0]),dbus.Byte(keyBytes[1])] }, [])
+        GLib.timeout_add(keyHold, self.sendNull)
+        
+    def sendNull(self):
+        self.PropertiesChanged(GATT_CHRC_IFACE, { 'Value': [dbus.Byte(0x00),dbus.Byte(0x00)] }, [])
+        return False
+                
     def ReadValue(self, options):
-        print(f'Read ProtocolMode: {self.value}')
+        print(f'Read Report: {self.value}')
         return self.value
 
-    def WriteValue(self, value, options):
-        print(f'Write ProtocolMode {value}')
-        self.value = value
+    def StartNotify(self):
+        print(f' \n***CONNECTED: Report Data to Client')
+        self.isConnected = True
+        #GLib.timeout_add(15000, self.send)
 
+    def StopNotify(self):
+        print(f' \n***DISCONNECTED: Report2 Client')
+        self.isConnected = False
+ 
 
-#id="hid_information" name="HID Information" sourceId="org.bluetooth.characteristic.hid_information" uuid="2A4A"
-class HIDInfoCharacteristic(gattApplication.Characteristic):
+#type="org.bluetooth.descriptor.report_reference" uuid="2908"
+class ReportReferenceDescriptor(gattApplication.Descriptor):
 
-    CHARACTERISTIC_UUID = '2A4A'
+    DESCRIPTOR_UUID = '2908'
 
-    def __init__(self, bus, index, service):
-        gattApplication.Characteristic.__init__(
+    def __init__(self, bus, index, characteristic):
+        gattApplication.Descriptor.__init__(
                 self, bus, index,
-                self.CHARACTERISTIC_UUID,
+                self.DESCRIPTOR_UUID,
                 ['read'],
-                service)
-
+                characteristic)
+                
         '''
-        <Field name="bcdHID">
-            <InformativeText>16-bit unsigned integer representing version number of base USB HID Specification implemented by HID Device</InformativeText>
+        <Field name="Report ID">
             <Requirement>Mandatory</Requirement>
-            <Format>uint16</Format>
+            <Format>uint8</Format>
+            <Minimum>0</Minimum>
+            <Maximum>255</Maximum>
         </Field>
         
-        <Field name="bCountryCode">
-            <InformativeText>Identifies which country the hardware is localized for. Most hardware is not localized and thus this value would be zero (0).</InformativeText>
+        <Field name="Report Type">
             <Requirement>Mandatory</Requirement>
-            <Format>8bit</Format>
-        </Field>
-        
-        <Field name="Flags">
-            <Requirement>Mandatory</Requirement>
-            <Format>8bit</Format>
-            <BitField>
-                <Bit index="0" size="1" name="RemoteWake">
-                <Enumerations>
-                    <Enumeration key="0" value="The device is not designed to be capable of providing wake-up signal to a HID host"/>
-                    <Enumeration key="1" value="The device is designed to be capable of providing wake-up signal to a HID host"/>
-                </Enumerations>
-                </Bit>
-            
-                <Bit index="1" size="1" name="NormallyConnectable">
-                <Enumerations>
-                    <Enumeration key="0" value="The device is not normally connectable"/>
-                    <Enumeration key="1" value="The device is normally connectable"/>
-                </Enumerations>
-                </Bit>
-            
-                <ReservedForFutureUse index="2" size="6"/>
-            </BitField>
+            <Format>uint8</Format>
+            <Minimum>1</Minimum>
+            <Maximum>3</Maximum>
+            <Enumerations>
+                <Enumeration value="Input Report" key="1"/>
+                <Enumeration value="Output report" key="2"/>
+                <Enumeration value="Feature Report" key="3"/>
+                <ReservedForFutureUse start="4" end="255"/>
+                <ReservedForFutureUse1 start1="0" end1="0"/>
+            </Enumerations>
         </Field>
         '''
         
-        self.value = dbus.Array(bytearray.fromhex('01110003'), signature=dbus.Signature('y'))
-        #print(f'***HIDInformation value***: {self.value}')
+        # This report uses ReportId 2 as defined in the ReportMap characteristic
+        self.value = dbus.Array(bytearray.fromhex('0201'), signature=dbus.Signature('y'))
+        #print(f'***ReportReference***: {self.value}')
 
     def ReadValue(self, options):
-        print(f'Read HIDInformation: {self.value}')
+        print(f'Read ReportReference: {self.value}')
         return self.value
-
-#sourceId="org.bluetooth.characteristic.hid_control_point" uuid="2A4C"
-class ControlPointCharacteristic(gattApplication.Characteristic):
-
-    CHARACTERISTIC_UUID = '2A4C'
-
-    def __init__(self, bus, index, service):
-        gattApplication.Characteristic.__init__(
-                self, bus, index,
-                self.CHARACTERISTIC_UUID,
-                ["write-without-response"],
-                service)
-        
-        self.value = dbus.Array(bytearray.fromhex('00'), signature=dbus.Signature('y'))
-        #print(f'***ControlPoint value***: {self.value}')
-
-    def WriteValue(self, value, options):
-        print(f'Write ControlPoint {value}')
-        self.value = value
-
 
 #sourceId="org.bluetooth.characteristic.report_map" uuid="2A4B"
 class ReportMapCharacteristic(gattApplication.Characteristic):
@@ -229,6 +213,118 @@ class ReportMapCharacteristic(gattApplication.Characteristic):
         self.isMapLoaded = True
         return self.value
 
+#id="hid_information" name="HID Information" sourceId="org.bluetooth.characteristic.hid_information" uuid="2A4A"
+class HIDInfoCharacteristic(gattApplication.Characteristic):
+
+    CHARACTERISTIC_UUID = '2A4A'
+
+    def __init__(self, bus, index, service):
+        gattApplication.Characteristic.__init__(
+                self, bus, index,
+                self.CHARACTERISTIC_UUID,
+                ['read'],
+                service)
+
+        '''
+        <Field name="bcdHID">
+            <InformativeText>16-bit unsigned integer representing version number of base USB HID Specification implemented by HID Device</InformativeText>
+            <Requirement>Mandatory</Requirement>
+            <Format>uint16</Format>
+        </Field>
+        
+        <Field name="bCountryCode">
+            <InformativeText>Identifies which country the hardware is localized for. Most hardware is not localized and thus this value would be zero (0).</InformativeText>
+            <Requirement>Mandatory</Requirement>
+            <Format>8bit</Format>
+        </Field>
+        
+        <Field name="Flags">
+            <Requirement>Mandatory</Requirement>
+            <Format>8bit</Format>
+            <BitField>
+                <Bit index="0" size="1" name="RemoteWake">
+                <Enumerations>
+                    <Enumeration key="0" value="The device is not designed to be capable of providing wake-up signal to a HID host"/>
+                    <Enumeration key="1" value="The device is designed to be capable of providing wake-up signal to a HID host"/>
+                </Enumerations>
+                </Bit>
+            
+                <Bit index="1" size="1" name="NormallyConnectable">
+                <Enumerations>
+                    <Enumeration key="0" value="The device is not normally connectable"/>
+                    <Enumeration key="1" value="The device is normally connectable"/>
+                </Enumerations>
+                </Bit>
+            
+                <ReservedForFutureUse index="2" size="6"/>
+            </BitField>
+        </Field>
+        '''
+        
+        self.value = dbus.Array(bytearray.fromhex('01110003'), signature=dbus.Signature('y'))
+        #print(f'***HIDInformation value***: {self.value}')
+
+    def ReadValue(self, options):
+        print(f'Read HIDInformation: {self.value}')
+        return self.value
+        
+#name="Protocol Mode" sourceId="org.bluetooth.characteristic.protocol_mode" uuid="2A4E"
+class ProtocolModeCharacteristic(gattApplication.Characteristic):
+
+    CHARACTERISTIC_UUID = '2A4E'
+
+    def __init__(self, bus, index, service):
+        
+        gattApplication.Characteristic.__init__(
+                self, bus, index,
+                self.CHARACTERISTIC_UUID,
+                ["read", "write-without-response"],
+                service)
+                
+        '''        
+        <Field name="Protocol Mode Value">
+        <Requirement>Mandatory</Requirement>
+        <Format>uint8</Format>
+        <Enumerations>
+        <Enumeration key="0" value="Boot Protocol Mode"/>
+        <Enumeration key="1" value="Report Protocol Mode"/>
+        <ReservedForFutureUse start="2" end="255"/>
+        </Enumerations>
+        '''
+        
+        #self.value = dbus.Array([1], signature=dbus.Signature('y'))
+        self.parent = service
+        self.value = dbus.Array(bytearray.fromhex('01'), signature=dbus.Signature('y'))
+        #print(f'***ProtocolMode value***: {self.value}')
+
+    def ReadValue(self, options):
+        print(f'Read ProtocolMode: {self.value}')
+        return self.value
+
+    def WriteValue(self, value, options):
+        print(f'Write ProtocolMode {value}')
+        self.value = value
+        
+'''
+#sourceId="org.bluetooth.characteristic.hid_control_point" uuid="2A4C"
+class ControlPointCharacteristic(gattApplication.Characteristic):
+
+    CHARACTERISTIC_UUID = '2A4C'
+
+    def __init__(self, bus, index, service):
+        gattApplication.Characteristic.__init__(
+                self, bus, index,
+                self.CHARACTERISTIC_UUID,
+                ["write-without-response"],
+                service)
+        
+        self.value = dbus.Array(bytearray.fromhex('00'), signature=dbus.Signature('y'))
+        #print(f'***ControlPoint value***: {self.value}')
+
+    def WriteValue(self, value, options):
+        print(f'Write ControlPoint {value}')
+        self.value = value
+
 
 #id="report" name="Report" sourceId="org.bluetooth.characteristic.report" uuid="2A4D"        
 class Report1Characteristic(gattApplication.Characteristic):
@@ -242,7 +338,8 @@ class Report1Characteristic(gattApplication.Characteristic):
                 ['read', 'notify'],
                 service)
                 
-        '''
+'''
+'''
         <Field name="Report Value">
         <Requirement>Mandatory</Requirement>
         <Format>uint8</Format>
@@ -250,7 +347,8 @@ class Report1Characteristic(gattApplication.Characteristic):
         </Field>
         
         Use standard key codes: https://www.usb.org/sites/default/files/documents/hut1_12v2.pdf
-        '''
+'''
+'''
         
         self.add_descriptor(Report1ReferenceDescriptor(bus, 1, self))
         
@@ -296,7 +394,8 @@ class Report1ReferenceDescriptor(gattApplication.Descriptor):
                 ['read'],
                 characteristic)
                 
-        '''
+'''
+'''
         <Field name="Report ID">
             <Requirement>Mandatory</Requirement>
             <Format>uint8</Format>
@@ -317,8 +416,8 @@ class Report1ReferenceDescriptor(gattApplication.Descriptor):
                 <ReservedForFutureUse1 start1="0" end1="0"/>
             </Enumerations>
         </Field>
-        '''
-       
+'''
+'''
         # This report uses ReportId 1 as defined in the ReportMap characteristic
         self.value = dbus.Array(bytearray.fromhex('0101'), signature=dbus.Signature('y'))
         #print(f'***ReportReference***: {self.value}')
@@ -326,103 +425,8 @@ class Report1ReferenceDescriptor(gattApplication.Descriptor):
     def ReadValue(self, options):
         print(f'Read ReportReference: {self.value}')
         return self.value
+'''
 
-
-#id="report" name="Report" sourceId="org.bluetooth.characteristic.report" uuid="2A4D"        
-class Report2Characteristic(gattApplication.Characteristic):
-
-    CHARACTERISTIC_UUID = '2A4D'
-
-    def __init__(self, bus, index, service):
-        gattApplication.Characteristic.__init__(
-                self, bus, index,
-                self.CHARACTERISTIC_UUID,
-                ['read', 'notify'],
-                service)
-                
-        '''
-        <Field name="Report Value">
-        <Requirement>Mandatory</Requirement>
-        <Format>uint8</Format>
-        <Repeated>true</Repeated>
-        </Field>
-        
-        Use standard key codes: https://www.usb.org/sites/default/files/documents/hut1_12v2.pdf
-        '''
-        
-        self.add_descriptor(Report2ReferenceDescriptor(bus, 1, self))
-        
-        self.isConnected = False
-        self.value = [dbus.Byte(0x00),dbus.Byte(0x00)]
-        #print(f'***Report value***: {self.value}')
-              
-    def sendKey(self, keyBytes, keyHold):
-        #send keyCode
-        print(f' \n***Send report2 keyCode: {[hex(x) for x in keyBytes]}, keyHold: {keyHold}***');
-        #if(self.isConnected == False): print('Abort Report2: Not connected to client'); return
-        self.PropertiesChanged(GATT_CHRC_IFACE, { 'Value': [dbus.Byte(keyBytes[0]),dbus.Byte(keyBytes[1])] }, [])
-        GLib.timeout_add(keyHold, self.sendNull)
-        
-    def sendNull(self):
-        self.PropertiesChanged(GATT_CHRC_IFACE, { 'Value': [dbus.Byte(0x00),dbus.Byte(0x00)] }, [])
-        return False
-                
-    def ReadValue(self, options):
-        print(f'Read Report: {self.value}')
-        return self.value
-
-    def StartNotify(self):
-        print(f' \n***CONNECTED: Report2 Client')
-        self.isConnected = True
-        #GLib.timeout_add(15000, self.send)
-
-    def StopNotify(self):
-        print(f' \n***DISCONNECTED: Report2 Client')
-        self.isConnected = False
- 
-
-#type="org.bluetooth.descriptor.report_reference" uuid="2908"
-class Report2ReferenceDescriptor(gattApplication.Descriptor):
-
-    DESCRIPTOR_UUID = '2908'
-
-    def __init__(self, bus, index, characteristic):
-        gattApplication.Descriptor.__init__(
-                self, bus, index,
-                self.DESCRIPTOR_UUID,
-                ['read'],
-                characteristic)
-                
-        '''
-        <Field name="Report ID">
-            <Requirement>Mandatory</Requirement>
-            <Format>uint8</Format>
-            <Minimum>0</Minimum>
-            <Maximum>255</Maximum>
-        </Field>
-        
-        <Field name="Report Type">
-            <Requirement>Mandatory</Requirement>
-            <Format>uint8</Format>
-            <Minimum>1</Minimum>
-            <Maximum>3</Maximum>
-            <Enumerations>
-                <Enumeration value="Input Report" key="1"/>
-                <Enumeration value="Output report" key="2"/>
-                <Enumeration value="Feature Report" key="3"/>
-                <ReservedForFutureUse start="4" end="255"/>
-                <ReservedForFutureUse1 start1="0" end1="0"/>
-            </Enumerations>
-        </Field>
-        '''
-        
-        # This report uses ReportId 2 as defined in the ReportMap characteristic
-        self.value = dbus.Array(bytearray.fromhex('0201'), signature=dbus.Signature('y'))
-        #print(f'***ReportReference***: {self.value}')
-
-    def ReadValue(self, options):
-        print(f'Read ReportReference: {self.value}')
-        return self.value
 
 #############################################
 def onConnectSignal(interface, changed, data=[]):
